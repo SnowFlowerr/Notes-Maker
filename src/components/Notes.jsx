cc1e2f80907a4564fc2e0c1ab199201abe8c018e
Bhudeo Krit <Bhudeokrit@gmail.com>
2024-03-17 00:15:32 +0530

    bug fixed

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -14,21 +14,14 @@ export default function Notes({ note, setNote }) {
     const [center, setCenter] = useState(false);
     const [print, setPrint] = useState(false);
     
-
-
     function handleChange(e) {
         e.preventDefault();
-        if (note.title !== "") {
             setNote({ ...note, [e.target.id]: e.target.value })
-        }
-        else {
-            alert("Write the Title first")
-        }
     }
     function handleBold(e) {
         e.preventDefault();
         setBold(!bold)
-        if (bold) {
+        if (!bold) {
             document.getElementById("text").style.fontWeight = "bold";
             document.getElementById("bold").style.backgroundColor = "lightgreen"
         }
@@ -40,7 +33,7 @@ export default function Notes({ note, setNote }) {
     function handleItalic(e) {
         e.preventDefault();
         setItalic(!italic)
-        if (italic) {
+        if (!italic) {
             document.getElementById("text").style.fontStyle = "italic";
             document.getElementById("italic").style.backgroundColor = "lightgreen"
         }
@@ -52,7 +45,7 @@ export default function Notes({ note, setNote }) {
     function handleUnderline(e) {
         e.preventDefault();
         setUnderline(!underline)
-        if (underline) {
+        if (!underline) {
             document.getElementById("text").style.textDecorationLine = "underline";
             document.getElementById("underline").style.backgroundColor = "lightgreen"
         }
@@ -64,22 +57,22 @@ export default function Notes({ note, setNote }) {
     function handleColor(e) {
         e.preventDefault();
         setColor1(!color1)
-        if (color1) {
-            document.getElementById("color").style.backgroundColor = "white"
+        if (!color1) {
+            document.getElementById("color").style.backgroundColor = "lightgreen";
         }
         else {
-            document.getElementById("color").style.backgroundColor = "lightgreen";
+            document.getElementById("color").style.backgroundColor = "white"
         }
     }
     function handleLeft(e) {
         e.preventDefault();
         setLeft(!left)
-        if (left) {
+        if (!left) {
             document.getElementById("text").style.textAlign = "left";
             document.getElementById("left").style.backgroundColor = "lightgreen"
-            setRight(true)
-            setCenter(true)
-            setJustify(true)
+            setRight(false)
+            setCenter(false)
+            setJustify(false)
             document.getElementById("right").style.backgroundColor = "white"
             document.getElementById("center").style.backgroundColor = "white"
             document.getElementById("justify").style.backgroundColor = "white"
@@ -91,12 +84,12 @@ export default function Notes({ note, setNote }) {
     function handleRight(e) {
         e.preventDefault();
         setRight(!right)
-        if (right) {
+        if (!right) {
             document.getElementById("text").style.textAlign = "right";
             document.getElementById("right").style.backgroundColor = "lightgreen"
-            setLeft(true)
-            setCenter(true)
-            setJustify(true)
+            setLeft(false)
+            setCenter(false)
+            setJustify(false)
             document.getElementById("left").style.backgroundColor = "white"
             document.getElementById("center").style.backgroundColor = "white"
             document.getElementById("justify").style.backgroundColor = "white"
@@ -108,12 +101,12 @@ export default function Notes({ note, setNote }) {
     function handleJustify(e) {
         e.preventDefault();
         setJustify(!justify)
-        if (justify) {
+        if (!justify) {
             document.getElementById("text").style.textAlign = "justify";
             document.getElementById("justify").style.backgroundColor = "lightgreen"
-            setRight(true)
-            setCenter(true)
-            setLeft(true)
+            setRight(false)
+            setCenter(false)
+            setLeft(false)
             document.getElementById("right").style.backgroundColor = "white"
             document.getElementById("center").style.backgroundColor = "white"
             document.getElementById("left").style.backgroundColor = "white"
@@ -125,12 +118,12 @@ export default function Notes({ note, setNote }) {
     function handleCenter(e) {
         e.preventDefault();
         setCenter(!center)
-        if (center) {
+        if (!center) {
             document.getElementById("text").style.textAlign = "center";
             document.getElementById("center").style.backgroundColor = "lightgreen"
-            setRight(true)
-            setLeft(true)
-            setJustify(true)
+            setRight(false)
+            setLeft(false)
+            setJustify(false)
             document.getElementById("right").style.backgroundColor = "white"
             document.getElementById("left").style.backgroundColor = "white"
             document.getElementById("justify").style.backgroundColor = "white"
@@ -142,7 +135,7 @@ export default function Notes({ note, setNote }) {
     function handlePrint(e) {
         e.preventDefault();
         setPrint(!print)
-        if (print) {
+        if (!print) {
             var printTextArea = document.getElementById("text");
             var WinPrint = window.open('left=0,top=0,width=800,height=900,');
             WinPrint.document.write(printTextArea.innerHTML);
@@ -157,11 +150,12 @@ export default function Notes({ note, setNote }) {
         setColor(e.target.value)
         document.getElementById("text").style.color = `#${color}`;
     }
-
+    
     return (
         <div className={styles.Notesbox}>
             <div className={styles.notesName}>
-                <u>{note.title === "" ? "Title" : note.title}</u>
+                
+                <label htmlFor="title"><u>{note.title === "" ? "Title" : note.title}</u></label>
             </div>
             <div className={styles.funBtn}>
                 <button className={styles.btn} onClick={handleBold} id="bold"><i class="fa-solid fa-bold"></i></button>
